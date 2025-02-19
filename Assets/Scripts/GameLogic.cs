911f966c9627d9e5f69a07c7b8a7be601bf2e8c9
YoungsterJoey123 <omeshjeevan1@gmail.com>
2025-02-19 08:57:35 +0100

    fwe
    egr

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,7 +1,33 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-        
+
+    int winnerFound = -1;
+    int dice = UnityEngine.Random.Range(1, 7);
+    Player[] players = new Player[4];
+    public GameLogic()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i] = new Player();
+        }
+        while (winnerFound < 0)
+        {
+            for (int i = 0; i < players.Length; i++)
+            {
+                int diceRoll = dice;
+                bool winner = players[i].DecideAndMovePiece(diceRoll);
+                if (winner)
+                {
+                    winnerFound = i;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine("winner is player " + winnerFound);
+    }
+
 }
--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class GamePiece : MonoBehaviour
 {
+    private int position = 0;
 
-    
+    public int Position { get => position; }
+
+    public void Move(int x)
+    {
+        position += x;
+    }
+
 }
