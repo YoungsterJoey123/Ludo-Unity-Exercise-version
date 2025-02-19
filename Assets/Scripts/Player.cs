681755b1630792557bcad477eedd0c6fc2ef79ae
YoungsterJoey123 <omeshjeevan1@gmail.com>
2025-02-19 08:29:57 +0100

    ehgf
    djbydi

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,5 +3,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    GamePiece[] pieces = new GamePiece[4];
+    public Player()
+    {
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            pieces[i] = new GamePiece();
+        }
+    }
+    public bool DecideAndMovePiece(int rollValue)
+    {
+        if (rollValue == 6)
+        {
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                if (pieces[i].Position == 0)
+                {
+                    pieces[i].Move(1);
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                if (pieces[i].Position + rollValue <= 40)
+                {
+                    pieces[i].Move(rollValue);
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            if (pieces[i].Position < 40)
+            {
+                pieces[i].Move(rollValue);
+                return false;
+            }
+        }
+        return true;
+    }
 }
