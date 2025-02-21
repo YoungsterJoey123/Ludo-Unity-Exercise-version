999b28a01e612870fc664e7a221531e0c2608102
YoungsterJoey123 <omeshjeevan1@gmail.com>
2025-02-21 12:31:40 +0100

    done
    ef

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,23 +3,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    GamePiece[] pieces = new GamePiece[4];
-    public Player()
+
+    GameObject[] pieces = new GameObject[4];
+    public void Start()
     {
         for (int i = 0; i < pieces.Length; i++)
         {
-            pieces[i] = new GamePiece();
+            pieces[i] = transform.GetChild(i).gameObject;
         }
     }
+
+
     public bool DecideAndMovePiece(int rollValue)
     {
         if (rollValue == 6)
         {
             for (int i = 0; i < pieces.Length; i++)
             {
-                if (pieces[i].Position == 0)
-                {
-                    pieces[i].Move(1);
+                if (pieces[i].transform.position.x == 0)
+                {                    
+                    pieces[i].GetComponent<GamePiece>().Move(1);
                     return false;
                 }
             }
@@ -28,18 +31,18 @@ public class Player : MonoBehaviour
         {
             for (int i = 0; i < pieces.Length; i++)
             {
-                if (pieces[i].Position + rollValue <= 40)
+                if (pieces[i].transform.position.x + rollValue <= 80)
                 {
-                    pieces[i].Move(rollValue);
+                    pieces[i].GetComponent<GamePiece>().Move(rollValue);
                     return false;
                 }
             }
         }
         for (int i = 0; i < pieces.Length; i++)
         {
-            if (pieces[i].Position < 40)
+            if (pieces[i].transform.position.x < 80)
             {
-                pieces[i].Move(rollValue);
+                pieces[i].GetComponent<GamePiece>().Move(rollValue);
                 return false;
             }
         }
