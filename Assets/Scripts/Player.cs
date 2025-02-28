307229a5a3ff3324398ea089eef97adfef21cf64
YoungsterJoey123 <omeshjeevan1@gmail.com>
2025-02-28 08:19:54 +0100

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,7 +31,7 @@ public class Player : MonoBehaviour
         {
             for (int i = 0; i < pieces.Length; i++)
             {
-                if (pieces[i].transform.position.x + rollValue <= 80)
+                if (pieces[i].transform.position.x + rollValue <= 80 && pieces[i].transform.position.x > 0)
                 {
                     pieces[i].GetComponent<GamePiece>().Move(rollValue);
                     return false;
